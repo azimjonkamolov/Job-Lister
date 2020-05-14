b55b4502288baa2c788aded09e1775b6eff95164
Azimjon Kamolov <azimjonkamolovuz@gmail.com>
2020-05-14 15:29:46 +0900

    DB done

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Test
+<?php
+
+    include_once "config/init.php"; 
+
+    $template = new Template("templates/frontpage.php");
+
+    $template->title = "Job Finder";
+    
+    
+    echo $template;
+
+?>
+
---
+++ templates/inc/footer.php
@@ -0,0 +1,7 @@
+
+        <footer class="container">
+            <p>&copy; Company 2017-2020</p>
+        </footer>
+        
+    </body>
+</html>
